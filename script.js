a547714a6f29f50592688dc61c5c0115f2f0a2b7
sahumo <7523460@gmail.com>
2021-08-24 20:22:34 +0300

    change global variable  inside function

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 'use strict';
 
+let num = 20;// global variable
+
 function showFirstMessage(text)
-{console.log(text);}
+{console.log(text);  num = 10;  }//num = 10; local variable 
+
+
+
 
 showFirstMessage("Hello world!");// takes argument from here and puts in line 3 instead text,after puts it in line 4 instead text
+console.log(num);
 
-
+// result---->  Hello world! 10
 
 
 
